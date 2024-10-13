c524d458bcc5068cbc13d66f8eec60e046ce63f8
Lanhpeo <lananh123lsls@gmail.com>
2024-10-13 22:48:06 +0700

    feat: login and register

--- lib/presentation/page/auth/signin_page.dart
+++ lib/presentation/page/auth/signin_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -8,8 +8,153 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign In"),
+      ),
+      backgroundColor: const Color(0xFFAE2C2C), // Màu nền nhẹ nhàng
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Image.asset("assets/title.png"),
+            const Text(
+              "Đăng nhập với tài khoản QLĐT",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            TextFormField(
+              controller: _emailController,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: const TextStyle(color: Colors.white),
+                prefixIcon: const Icon(Icons.email, color: Colors.white),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+
+            TextFormField(
+              controller: _passwordController,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: const TextStyle(color: Colors.white),
+                prefixIcon: const Icon(Icons.lock, color: Colors.white),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+              ),
+              obscureText: _obscurePassword, // Ẩn mật khẩu
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                // Xử lý sự kiện khi nhấn nút đăng nhập
+                String email = _emailController.text;
+                String password = _passwordController.text;
+
+                // In ra thông tin đăng nhập (có thể thay bằng logic đăng nhập thực tế)
+                print('Email: $email');
+                print('Password: $password');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 80, vertical: 16), // Kích thước nút
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8), // Bo góc nút
+                ),
+              ),
+              child: const Text(
+                'Đăng nhập',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xFFAE2C2C), // Màu chữ nút
+                ),
+              ),
+            ),
+            const SizedBox(height: 24), // Khoảng cách trước nút đăng nhập
+
+            Align(
+              alignment: Alignment.center,
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/ForgotPassword');
+                },
+                child: const Text(
+                  'Quên mật khẩu?',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white, // Màu chữ trắng cho liên kết
+                    decoration:
+                        TextDecoration.underline, // Thêm gạch chân cho liên kết
+                    decorationColor: Colors.white, // Màu gạch chân trắng
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 24), // Khoảng cách trước nút đăng nhập
+
+            Align(
+              alignment: Alignment.center,
+              child: InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, '/SignUpPage');
+                },
+                child: const Text(
+                  'Chưa có tải khoản: Đăng ký',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white, // Màu chữ trắng cho liên kết
+                    decoration:
+                        TextDecoration.underline, // Thêm gạch chân cho liên kết
+                    decorationColor: Colors.white, // Màu gạch chân trắng
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/presentation/page/splash_page.dart
+++ lib/presentation/page/splash_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -10,10 +12,92 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, '/HomePage');
-        },
-        child: Center(child: const Text("Splash Page")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Splash Page", style: TextStyle(fontSize: 24)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/SignUpPage');
+              },
+              child: const Text("Go to SignUpPage"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/SignInPage');
+              },
+              child: const Text("Go to SignInPage"),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: const Color(0xFFAE2C2C),
+          // Thay đổi màu nền của BottomNavigationBar
+          items: [
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/notification.svg",
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                label: '',
+                backgroundColor: Color(0xFFAE2C2C)),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/group.svg",
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                label: '',
+                backgroundColor: Color(0xFFAE2C2C)),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                "",
+                width: 30,
+                height: 30,
+                fit: BoxFit.fill,
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/chat-round-dots.svg",
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                label: '',
+                backgroundColor: Colors.transparent),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  "assets/setting.svg",
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                label: '',
+                backgroundColor: Colors.transparent),
+          ]),
+      floatingActionButton: Container(
+        width: 64,
+        height: 64,
+        decoration: BoxDecoration(
+            color: const Color(0xFF2DB787),
+            borderRadius: BorderRadius.circular(32)),
+        child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.add,
+              size: 30,
+              color: Colors.white,
+            )),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
