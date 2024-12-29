422b0cfbd868409a2ba06afc8026d614d9e2d41b
thanh <thanhdepzai37@gmail.com>
2024-12-29 14:42:57 +0700

    add fcm token to login api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 import 'package:qldt/data/model/user.dart';
 import 'package:qldt/data/remote/api_service_it4788.dart';
