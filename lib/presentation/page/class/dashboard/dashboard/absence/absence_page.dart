c599da7674b5712945491536516d98804b98cf27
Lanhpeo <lananh123lsls@gmail.com>
2024-12-30 20:39:12 +0700

    fix bug trong list

--- lib/presentation/page/class/dashboard/dashboard/absence/absence_page.dart
+++ lib/presentation/page/class/dashboard/dashboard/absence/absence_page.dart
@@ -222,11 +222,14 @@ import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
+import 'package:qldt/data/model/class.dart';
 import 'package:qldt/data/repo/absence_repository.dart';
 import 'package:qldt/data/request/absence_request.dart'; // Import AbsenceRequest
 
 import '../../../../../pref/user_preferences.dart';
+import '../../../class_list_provider.dart';
 import 'absence_provider.dart';  // Your AbsenceProvider
 
 
@@ -241,6 +244,8 @@ class AbsencePage extends StatefulWidget {
 
 
 class _AbsencePageState extends State<AbsencePage> {
+  late final ClassListProvider _classProvider;
+  List<Class>? _classList;
   final _formKey = GlobalKey<FormState>();
   List<AbsenceFileRequest> files = [];
   final TextEditingController _reasonController = TextEditingController();
@@ -248,6 +253,18 @@ class _AbsencePageState extends State<AbsencePage> {
 
   DateTime? _selectedDate;
 
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      _classProvider = context.read<ClassListProvider>();
+
+      // Gọi `getClassList` và lưu kết quả vào `_classList`
+      _classList = await _classProvider.getClassList();
+      setState(() {}); // Cập nhật giao diện sau khi lấy dữ liệu
+    });
+  }
   // Pick file function (unchanged)
   Future<void> pickFile() async {
     final result = await FilePicker.platform.pickFiles(allowMultiple: true);
@@ -296,7 +313,7 @@ class _AbsencePageState extends State<AbsencePage> {
     final pickedDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
-      firstDate: DateTime(2023),
+      firstDate: DateTime.now() ,
       lastDate: DateTime(2025),
     );
 
@@ -310,6 +327,37 @@ class _AbsencePageState extends State<AbsencePage> {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<AbsenceProvider>(context);
+    final classProvider = Provider.of<ClassListProvider>(context);
+
+
+    // Tìm lớp phù hợp với widget.classId
+    final matchingClass = classProvider.classes?.firstWhere(
+          (c) => c.id == widget.classId,
+      orElse: () =>
+          Class(id: "0",
+              name: "0",
+              attachedCode: "0",
+              lecturerName: "0",
+              classType: "0",
+              studentCount: "0",
+              startDate: "0",
+              endDate: "0",
+              status: "0"),
+    );
+    bool isClassExpired = false;
+    if (matchingClass != null && matchingClass.endDate != "0") {
+      final classEndDate = DateTime.tryParse(matchingClass.endDate ?? "");
+      if (classEndDate != null && classEndDate.isBefore(DateTime.now())) {
+        isClassExpired = true;
+      }
+    }
+    // if (matchingClass != null) {
+    //   Logger().d("End date of class ${widget.classId}: ${matchingClass.endDate}");
+    // } else {
+    //   Logger().d("No class found with class_id ${widget.classId}");
+    // }
+
+
 
     return Scaffold(
       appBar: AppBar(
@@ -318,81 +366,90 @@ class _AbsencePageState extends State<AbsencePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 10),
-                GestureDetector(
-                  onTap: pickDate,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 12.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Text(
-                      _selectedDate != null
-                          ? _selectedDate!.toIso8601String().split("T")[0]
-                          : 'Select Date',
-                      style: const TextStyle(fontSize: 16.0),
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (isClassExpired)
+              // Display the expiration message if the class is expired
+                Center(
+                  child: Text(
+                    'Lớp học đã quá hạn',
+                    style: TextStyle(fontSize: 18, color: Colors.red, fontWeight: FontWeight.bold),
                   ),
-                ),
-                if (_selectedDate == null)
-                  const Padding(
-                    padding: EdgeInsets.only(top: 5.0),
-                    child: Text(
-                      'Date is required',
-                      style: TextStyle(color: Colors.red, fontSize: 12),
-                    ),
-                  ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  controller: _titleController,
-                  decoration: const InputDecoration(labelText: 'Title'),
-                  validator: (value) =>
-                  value == null || value.isEmpty ? 'Title is required' : null,
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  controller: _reasonController,
-                  decoration: const InputDecoration(labelText: 'Reason'),
-                  validator: (value) =>
-                  value == null || value.isEmpty ? 'Reason is required' : null,
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: pickFile,
-                  child: Text(files.isEmpty ? 'Tải minh chứng' : 'Change File'),
-                ),
-                if (files.isNotEmpty) Column(
+                )
+              else
+              // Display the rest of the form if the class is not expired
+                Column(
                   children: [
-                    for (var i = 0; i < files.length; i++)
-                    Text('File: ${files[i].file?.name?.split('/').last}'),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: pickDate,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        child: Text(
+                          _selectedDate != null
+                              ? _selectedDate!.toIso8601String().split("T")[0]
+                              : 'Select Date',
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                      ),
+                    ),
+                    if (_selectedDate == null)
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Text(
+                          'Date is required',
+                          style: TextStyle(color: Colors.red, fontSize: 12),
+                        ),
+                      ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(labelText: 'Title'),
+                      validator: (value) =>
+                      value == null || value.isEmpty ? 'Title is required' : null,
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _reasonController,
+                      decoration: const InputDecoration(labelText: 'Reason'),
+                      validator: (value) =>
+                      value == null || value.isEmpty ? 'Reason is required' : null,
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: pickFile,
+                      child: Text(files.isEmpty ? 'Tải minh chứng' : 'Change File'),
+                    ),
+                    if (files.isNotEmpty) Column(
+                      children: [
+                        for (var i = 0; i < files.length; i++)
+                          Text('File: ${files[i].file?.name?.split('/').last}'),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_selectedDate == null) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Please select a date')),
+                          );
+                          return;
+                        }
+                        submitAbsenceRequest(provider);
+                      },
+                      child: provider.isLoading
+                          ? const CircularProgressIndicator()
+                          : const Text('Gửi'),
+                    ),
+                    const SizedBox(height: 20),
                   ],
                 ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_selectedDate == null) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Please select a date')),
-                      );
-                      return;
-                    }
-                    submitAbsenceRequest(provider);
-                  },
-                  child: provider.isLoading
-                      ? const CircularProgressIndicator()
-                      : const Text('Gửi'),
-                ),
-                const SizedBox(height: 20),
-                // You can display additional data or a list of absences here
-              ],
-            ),
+            ],
           ),
         ),
       ),
